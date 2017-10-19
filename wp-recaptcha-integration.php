23f57ea54f46cf6875b47d4821313d9671cf8ae6
mcguffin <joern@flyingletters.com>
2017-10-19 19:31:36 +0200

    version increment

--- wp-recaptcha-integration.php
+++ wp-recaptcha-integration.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: WP reCaptcha Integration
 Plugin URI: https://wordpress.org/plugins/wp-recaptcha-integration/
-Description: Integrate reCaptcha in your blog. Supports no Captcha (new style recaptcha) as well as the old style reCaptcha. Provides of the box integration for signup, login, comment forms and lost password.
-Version: 1.3.0
+Description: Integrate reCaptcha in your blog. Supports no Captcha (new style recaptcha). Provides of the box integration for signup, login, comment forms and lost password.
+Version: 1.3.1
 Author: Jörn Lund
 Author URI: https://github.com/mcguffin/
 Text Domain: wp-recaptcha-integration
